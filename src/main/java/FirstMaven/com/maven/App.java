cd9f862d074bc87b4a9fb7270b9cc56138d5ac98
Albie93 <albiestar779@gmail.com>
2017-04-12 23:27:36 +0530

    First commit

---
+++ src/main/java/FirstMaven/com/maven/App.java
@@ -0,0 +1,15 @@
+package FirstMaven.com.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Calculation cal = new Calculation();
+        cal.addition(55, 45);
+    }
+}
---
+++ src/main/java/FirstMaven/com/maven/Calculation.java
@@ -0,0 +1,14 @@
+package FirstMaven.com.maven;
+
+public class Calculation {
+
+	
+	public void addition(int num1, int num2){
+		
+		int sum = num1+num2;
+		System.out.println("Addition Of two Numbers:"+sum);
+		
+	}
+	
+	
+}
